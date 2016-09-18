8ec7780346c7d316706903ecb3df8c975d15ccd8
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2016-09-17 17:32:36 -0700

    Adding emacs build script

---
+++ build-emacs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Build latest version of Emacs, version management with stow
+# OS: Ubuntu 14.04 LTS and newer
+# version: 24.5
+# Toolkit: lucid
+# Assumes that all dependencies are available.
+# Dependencies can be installed with:
+# sudo apt-get -qq install -y stow build-essential libx11-dev xaw3dg-dev \
+#      libjpeg-dev libpng12-dev libgif-dev libtiff5-dev libncurses5-dev \
+#      libxft-dev librsvg2-dev libmagickcore-dev libmagick++-dev \
+#      libxml2-dev libgpm-dev libotf-dev libm17n-dev \
+#      libgnutls-dev wget
+
+version="$1"
+cpu_count=`grep -c ^processor /proc/cpuinfo`
+build_threads=$(( $cpu_count * 2 ))
+
+echo "CPU count is probably $cpu_count, setting build_threads to $build_threads"
+
+if [ -z "$version" ]; then
+    echo "Version not specified, defaulting to 24.5"
+    version="24.5"
+fi
+
+# download source package
+if [[ ! -d emacs-"$version" ]]; then
+    wget http://ftp.gnu.org/gnu/emacs/emacs-"$version".tar.xz
+    tar xf emacs-"$version".tar.xz
+fi
+
+cd emacs-"$version"
+
+# build and install
+if [ "$version" > "25" ] ; then
+    ./configure \
+        --with-xft \
+        --with-x-toolkit=lucid \
+        --with-modules
+else
+    ./configure \
+        --with-xft \
+        --with-x-toolkit=lucid
+fi
+
+make -j"$build_threads"
+
+sudo mkdir -p /usr/local/stow
+sudo make \
+     install-arch-dep \
+     install-arch-indep \
+     prefix=/usr/local/stow/emacs-"$version"
+
+cd /usr/local/stow
+sudo stow emacs-"$version"
