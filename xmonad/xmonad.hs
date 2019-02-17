4398dd4753f84f1d694e3b9334b1b1ebe12c97fa
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-02-16 18:17:52 -0800

    xmonad config

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,68 @@
+import XMonad
+import XMonad.Actions.CycleWS
+import XMonad.Actions.Navigation2D
+import XMonad.Config.Desktop
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+
+import qualified XMonad.StackSet as W
+
+rofi = "rofi -show run"
+
+--------------------------------------------------------------------------------
+-- Possible mod keys
+-- mod1 - Left Alt
+-- mod2 - NUMLOCK
+-- mod3 
+
+
+myModMask = mod4Mask -- changes the mod key to "super"
+
+myKeys baseConfig@(XConfig {modMask = modKey}) =
+  -- ctrl-[1..9] %! Switch to workspace N
+  -- ctrl-shift-[1..9] %! Move client to workspace N
+  -- ctrl-shift-win-[1..9] %! Move client and switch to workspace N
+  [((m .|. controlMask, k), windows $ f i)
+      | (i, k) <- zip (XMonad.workspaces baseConfig) [xK_1 .. xK_9]
+      , (f, m) <- [(W.greedyView, 0)
+                  ,(W.shift, shiftMask)
+                  ,(\i -> W.greedyView i . W.shift i, shiftMask .|. mod4Mask)
+                  ]]
+
+-- TODO: Add dynamicLogWithPP to xmobar, take a look at https://github.com/davidbrewer/xmonad-ubuntu-conf/blob/master/xmonad.hs
+myConfig = desktopConfig
+  { terminal   = "kitty"
+  , modMask    = myModMask -- super
+  , layoutHook = desktopLayoutModifiers $ myLayouts
+  , workspaces = map show [ 1 .. 9 :: Int ]
+  }
+
+main = do
+  xmobarProc <- spawnPipe "~/.local/bin/xmobar ~/.xmobarrc"
+  -- floatNextWindows <- newIORef 0
+  xmonad $ myConfig
+    -- remove default modMask + [1 - 9] binding for switching workspaces
+    `removeKeys` [(mod4Mask, n) | n <- [xK_1 .. xK_9]]
+    -- remove modMask + SHIFT + [1 - 9] binding for flinging crap around workspaces
+    `removeKeys` [(mod4Mask .|. shiftMask, n) | n <- [xK_1 .. xK_9]]
+    -- add CTRL + [1 - 9] for switching workspaces
+    `additionalKeys` (myKeys myConfig)
+    `additionalKeysP`
+    [ ("M4-<Space>", spawn $ rofi)
+    , ("M-h", windowGo L False)
+    , ("M-j", windowGo D False)
+    , ("M-k", windowGo U False)
+    , ("M-l", windowGo R False)
+    , ("C-M-h", windowSwap L False)
+    , ("C-M-j", windowSwap D False)
+    , ("C-M-k", windowSwap U False)
+    , ("C-M-l", windowSwap R False)
+    ]
+
+--------------------------------------------------------------------------------
+-- | Desktop layouts
+myLayouts = emptyBSP
+
+--------------------------------------------------------------------------------
+-- | Keyboard layout
