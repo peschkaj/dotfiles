0671e8795b3850c34c3465c1513a8884d0da8512
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-03-08 09:12:32 -0800

    Removing personal and pdx email from sync

--- orgCalSync.sh
+++ orgCalSync.sh
@@ -2,19 +2,19 @@
 
 # customize these
 WGET="/usr/bin/env wget"
-ICS2ORG=/home/jeremiah/.local/bin/ical2orgpy
-ICSFILE=/home/jeremiah/Documents/org/calsync/jeremiahpeschka.ics
-ORGFILE=/home/jeremiah/Documents/org/calsync/jeremiahpeschka-cal.org
-URL=https://calendar.google.com/calendar/ical/jeremiah.peschka%40gmail.com/private-a1288e9305374ef03e7c555a8074ad88/basic.ics
+# ICS2ORG=/home/jeremiah/.local/bin/ical2orgpy
+# ICSFILE=/home/jeremiah/Documents/org/calsync/jeremiahpeschka.ics
+# ORGFILE=/home/jeremiah/Documents/org/calsync/jeremiahpeschka-cal.org
+# URL=https://calendar.google.com/calendar/ical/jeremiah.peschka%40gmail.com/private-a1288e9305374ef03e7c555a8074ad88/basic.ics
 
-# no customization needed below
+# # no customization needed below
 
-logger -t "com.facility9.orgCalSync" "syncing personal email"
-echo "#+CATEGORY: personal" > $ORGFILE
-$WGET -O $ICSFILE $URL
-$ICS2ORG -d 180 $ICSFILE - >> $ORGFILE
-rm -f $ICSFILE
-logger -t "com.facility9.orgCalSync" "syncing personal email complete"
+# logger -t "com.facility9.orgCalSync" "syncing personal email"
+# echo "#+CATEGORY: personal" > $ORGFILE
+# $WGET -O $ICSFILE $URL
+# $ICS2ORG -d 180 $ICSFILE - >> $ORGFILE
+# rm -f $ICSFILE
+# logger -t "com.facility9.orgCalSync" "syncing personal email complete"
 
 
 ICSFILE=/home/jeremiah/Documents/org/calsync/legitbiz.ics
@@ -29,13 +29,13 @@ rm -f $ICSFILE
 logger -t "com.facility9.orgCalSync" "syncing legitbiz email complete"
 
 
-ICSFILE=/home/jeremiah/Documents/org/calsync/jpeschka.ics
-ORGFILE=/home/jeremiah/Documents/org/calsync/jpeschka-cal.org
-URL=https://calendar.google.com/calendar/ical/jpeschka%40pdx.edu/private-4d103f01e3d26f17c5a520f4dcbf0dd9/basic.ics
+# ICSFILE=/home/jeremiah/Documents/org/calsync/jpeschka.ics
+# ORGFILE=/home/jeremiah/Documents/org/calsync/jpeschka-cal.org
+# URL=https://calendar.google.com/calendar/ical/jpeschka%40pdx.edu/private-4d103f01e3d26f17c5a520f4dcbf0dd9/basic.ics
 
-logger -t "com.facility9.orgCalSync" "syncing pdx email"
-echo "#+CATEGORY: pdx.edu" > $ORGFILE
-$WGET -O $ICSFILE $URL
-$ICS2ORG -d 90 $ICSFILE - >> $ORGFILE
-rm -f $ICSFILE
-logger -t "com.facility9.orgCalSync" "syncing pdx complete"
+# logger -t "com.facility9.orgCalSync" "syncing pdx email"
+# echo "#+CATEGORY: pdx.edu" > $ORGFILE
+# $WGET -O $ICSFILE $URL
+# $ICS2ORG -d 90 $ICSFILE - >> $ORGFILE
+# rm -f $ICSFILE
+# logger -t "com.facility9.orgCalSync" "syncing pdx complete"
