54434cf70fb8c2c6cd7168d69bb2cddbddec7159
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-06-17 12:16:17 -0700

    Fixes emc.sh to open the first arg

--- emc.sh
+++ emc.sh
@@ -5,8 +5,8 @@ x=`emacsclient --alternate-editor '' --eval '(x-display-list)' 2>/dev/null`
 
 if [ -z "$x" ] || [ "$x" = "nil" ] ;then
     # Create one if there is no X window yet.
-    emacsclient --alternate-editor emacs --create-frame --no-wait
+    emacsclient --alternate-editor emacs --create-frame --no-wait $1
 else
     # prevent creating another X frame if there is at least one present.
-    emacsclient --alternate-editor emacs --create-frame --no-wait
+    emacsclient --alternate-editor emacs --create-frame --no-wait $1
 fi
