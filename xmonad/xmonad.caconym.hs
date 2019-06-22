7ff638b5513cb5dadd1fbdde63e2d8f1b1e343d7
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-06-21 19:08:16 -0700

    Adds more fun keybinds for that emacs lyfe

--- xmonad/xmonad.caconym.hs
+++ xmonad/xmonad.caconym.hs
@@ -1,6 +1,7 @@
 import           XMonad
 import           XMonad.Actions.CycleWS
 import           XMonad.Actions.Navigation2D
+import           XMonad.Actions.Submap
 import           XMonad.Config.Desktop
 import           XMonad.Hooks.DynamicLog        ( dynamicLogWithPP
                                                 , xmobarPP
@@ -31,12 +32,13 @@ import           Data.List                      ( isPrefixOf )
 import           Data.Monoid                    ( Endo
                                                 , mconcat
                                                 )
+import qualified Data.Map                      as M
 
 rofi = "rofi -show drun -modi drun"
 rofiRunCommand = "rofi -show run -modi run"
 rofiCalc = "rofi -show calc -modi calc -no-show-match -no-sort"
-rofiClip
-  = "rofi -modi \"clipboard:greenclip print\" -show clipboard -run-command '{cmd}'"
+rofiClip =
+  "rofi -modi \"clipboard:greenclip print\" -show clipboard -run-command '{cmd}'"
 rofiPower =
   "/home/jeremiah/.local/bin/rofi-power \"/home/jeremiah/.local/bin/stop\""
 rofiCharpicker = "/home/jeremiah/src/charpicker/charpicker.sh"
@@ -57,6 +59,7 @@ myKeys baseConfig@(XConfig { modMask = modKey }) =
     , (\i -> W.greedyView i . W.shift i, shiftMask .|. mod4Mask)
     ]
   ]
+
 
 --------------------------------------------------------------------------------
 -- | Desktop layouts
@@ -83,9 +86,7 @@ myManageHook = composeAll
   [ resource =? "desktop_window" --> doIgnore
   , className =? "Galculator" --> doFloat
   , className =? "Steam" --> doFloat
-  , className
-  =?  "steam"
-  --> doFullFloat  -- bigpicture-mode
+  , className =? "steam" --> doFullFloat  -- bigpicture-mode
   , className =? "Gimp" --> doFloat
   , className =? "stalonetray" --> doIgnore
   , isDialog --> doCenterFloat
@@ -126,28 +127,60 @@ main = do
     -- Unset the quit xmonad command because we want a sane shutdown
     `removeKeys`      [(myModMask .|. shiftMask, xK_q)]
     `additionalKeys`  (myKeys myConfig)
-    `additionalKeysP` [ ("M-S-q", spawn "/home/jeremiah/.local/bin/stop")
-                      , ("M4-<Space>"             , spawn $ rofi)
-                      , ("M4-r"                   , spawn $ rofiRunCommand)
-                      , ("M4-h"                   , windowGo L False)
-                      , ("M4-j"                   , windowGo D False)
-                      , ("M4-k"                   , windowGo U False)
-                      , ("M4-l"                   , windowGo R False)
-                      , ("C-M4-h"                 , windowSwap L False)
-                      , ("C-M4-j"                 , windowSwap D False)
-                      , ("C-M4-k"                 , windowSwap U False)
-                      , ("C-M4-l"                 , windowSwap R False)
+    `additionalKeysP` [ ("M-S-q"     , spawn "/home/jeremiah/.local/bin/stop")
+                      , ("M4-<Space>", spawn $ rofi)
+                      , ( "M4-r"
+                        , spawn $ rofiRunCommand
+                        )
+                      -- launch a terminal!
+                      , ( "C-. t"
+                        , spawn $ XMonad.terminal myConfig
+                        )
+                      -- Maps movement to both vim keys
+                      , ("M4-h"   , windowGo L False)
+                      , ("M4-j"   , windowGo D False)
+                      , ("M4-k"   , windowGo U False)
+                      , ("M4-l"   , windowGo R False)
+                      , ("C-M4-h" , windowSwap L False)
+                      , ("C-M4-j" , windowSwap D False)
+                      , ("C-M4-k" , windowSwap U False)
+                      , ("C-M4-l" , windowSwap R False)
                       , ("M4-M1-5", spawn $ "shutter -s --profile=default")
-                      , ("M4-c"                   , spawn $ rofiCharpicker)
-                      , ("M4-S-c"                 , spawn $ rofiClip)
-                      , ("M-p"                    , spawn $ rofiPower)
+                      , ("M4-c"   , spawn $ rofiCharpicker)
+                      , ("M4-S-c" , spawn $ rofiClip)
+                      , ("M-p"    , spawn $ rofiPower)
+                      , ( "M-l"
+                        , spawn "mate-screensaver-command -l"
+                        )
+                      -- Move windows around by hand
                       , ("M-r"                    , sendMessage Rotate)
+                      , ("C-. w r"                , sendMessage Rotate)
                       , ("M-s"                    , sendMessage Swap)
-                      , ("M-l", spawn "mate-screensaver-command -l")
-                      , ("M-n"                    , sendMessage FocusParent)
-                      , ("M-C-n"                  , sendMessage SelectNode)
-                      , ("M-S-n"                  , sendMessage MoveNode)
+                      , ("C-. w s"                , sendMessage Swap)
+                      , ("C-. w p"                , sendMessage FocusParent)
+                      , ("C-. w <Space>"          , sendMessage SelectNode)
+                      , ("C-. w y"                , sendMessage MoveNode)
                       , ("<XF86MonBrightnessUp>"  , spawn "light -A 5")
                       , ("<XF86MonBrightnessDown>", spawn "light -U 5")
+                      ]
+    `additionalKeys`  [ ( (controlMask, xK_period)
+                        , submap
+                        .  M.fromList
+                        $  [ ( (0, xK_w)
+                             , submap
+                             . M.fromList
+                             $ [ ((0, k), windows $ W.shift i)
+                               | (i, k) <- zip (XMonad.workspaces myConfig)
+                                               [xK_1 .. xK_9]
+                               ]
+                             )
+                           ]
+                        -- emacs-ish keys for switching windows C-. [b,f,p,n]
+                        ++ [ ((0, xK_b), windowGo L False)
+                           , ((0, xK_n), windowGo D False)
+                           , ((0, xK_p), windowGo U False)
+                           , ((0, xK_f), windowGo R False)
+                           ]
+                        )
                       ]
 
