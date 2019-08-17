d9ae394d9173580bd5ea9cb857e72f65d0e910c7
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2019-08-16 20:28:05 -0700

    mu4e configuration

--- corgmacs-custom.el
+++ corgmacs-custom.el
@@ -20,5 +20,198 @@
   :ensure t
   :config (paradox-enable))
 
+(load-theme 'zerodark t)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; mu4e configuration
+(use-package mu4e
+  :ensure nil
+  :config
+  (setq mu4e-maildir "/home/jeremiah/.mail"
+        mail-user-agent 'mu4e-user-agent
+        mu4e-view-show-images t
+        mu4e-html2text-command "w3m -dump -T text/html"
+        mu4e-use-fancy-chars t
+        mu4e-attachment-dir "~/Downloads"
+        mu4e-sent-folder "/sent"
+        mu4e-drafts-folder "/drafts"
+        ;; user-mail-address "jeremiah@legit.biz"
+        ;; smtpmail-default-smtp-server "smtp.fastmail.com"
+        ;; smtpmail-smtp-server "smtp.fastmail.com"
+        ;; smtpmail-smtp-service 465
+        ;; smtpmail-queue-dir (expand-file-name "~/.mail/queue")
+        mu4e-completing-read-function 'completing-read
+        ;; please close messages after sending...
+        mu4e-conversation-kill-buffer-on-exit t
+        message-kill-buffer-on-exit t
+
+        ;; Pick the first context on opening mu4e
+        mu4e-context-policy 'pick-first
+
+        ;; nobody needs to confirm quitting
+        mu4e-confirm-quit nil
+
+        ;; msmtp magical incantation
+        message-sendmail-f-is-evil 't
+        )
+
+  ;; Choose account label to feed msmtp -a option based on From header
+  ;; in Message buffer; This function must be added to
+  ;; message-send-mail-hook for on-the-fly change of From address before
+  ;; sending message since message-send-mail-hook is processed right
+  ;; before sending message.
+  (defun choose-msmtp-account ()
+    (if (message-mail-p)
+        (save-excursion
+          (let*
+              ((from (save-restriction
+                       (message-narrow-to-headers)
+                       (message-fetch-field "from")))
+               (account
+                (cond
+                 ((string-match "jeremiah@legit.biz" from) "legit.biz")
+                 ((string-match "jpeschka@pdx.edu" from) "pdx.edu"))))
+            (setq message-sendmail-extra-arguments (list '"-a" account))))))
+  (add-hook 'message-send-mail-hook 'choose-msmtp-account)
+
+
+
+  )
+  ;; Configure sending mail
+(setq ;send-mail-function 'sendmail-send-it
+      message-send-mail-function 'message-send-mail-with-sendmail
+      mail-specify-envelope-from t
+      message-sendmail-envelope-from 'header
+      mail-envelope-from 'header
+      sendmail-program "/usr/bin/msmtp"
+      user-full-name "Jeremiah Peschka")
+;; (use-package async
+;;   :ensure t
+;;   :config
+;;   (require 'smtpmail-async)
+;;   (setq
+;;    ;; Configure async mail sending
+;;    send-mail-function 'async-smtpmail-send-it
+;;    message-send-mail-function 'async-smtpmail-send-it))
+
+
+;; (use-package helm-mu
+;;   :ensure t
+;;   :after mu4e)
+
+(use-package mu4e-conversation
+  :ensure t
+  :after mu4e
+  :config
+  (global-mu4e-conversation-mode)
+  (setq mu4e-conversation-print-function 'mu4e-conversation-print-tree)
+  (add-hook 'mu4e-conversation-hook 'flyspell-mode))
+
+(setq mu4e-contexts
+      `( ,(make-mu4e-context
+           :name "legit.biz"
+           :match-func (lambda (msg)
+                         (when msg (string-prefix-p "/legit.biz" (mu4e-message-field msg :maildir))))
+           :vars '((mu4e-trash-folder . "/legit.biz/Trash")
+                   (mu43-reflie-folder . "/legit.biz/Archive")))
+         ,(make-mu4e-context
+           :name "pdx.edu"
+           :match-func (lambda (msg)
+                         (when msg (string-prefix-p "/pdx.edu" (mu4e-message-field msg :maildir))))
+           :vars '((mu4e-trash-folder . "/pdx.edu/[Gmail].Trash")
+                   (mu4e-refile-folder . "/pdx.edu/[Gmail].Archive")
+                   ))
+         ))
+
+
+
+(defun async-smtpmail-send-queued-mail (sync-func &rest args)
+"Send email without locking up Emacs. Argument SYNC-FUNC and ARGS should appear here."
+  (message "Starting asynchronous smtpmail-send-queued-mail")
+  (async-start
+   `(lambda ()
+      (require 'smtpmail)
+      ;; see smtpmail-async.el - we inject the same variables
+      ,(async-inject-variables
+        "\\`\\(smtpmail\\|async-smtpmail\\|\\(user-\\)?mail\\)-\\|auth-sources\\|epg\\|nsm"
+        nil
+        "\\`\\(mail-header-format-function\\|smtpmail-address-buffer\\|mail-mode-abbrev-table\\)")
+      ;; if we don't use the above inject we can pass in specific variables like this:
+      ;; (setq smtpmail-queue-dir ,smtpmail-queue-dir)
+      ;; (setq smtpmail-smtp-server ,smtpmail-smtp-server)
+      (,sync-func))
+   (lambda (&optional _unused)
+     (message "Done sending queued mail in the background."))))
+
+;; https://emacs.stackexchange.com/a/14827/8743 has more, err, advice.
+;(advice-add #'smtpmail-send-queued-mail :around #'async-smtpmail-send-queued-mail)
+
+(setq my-mu4e-account-alist
+  '(("legit.biz"
+     (mu4e-sent-folder "/legit.biz/Sent")
+     (user-mail-address "jeremiah@legit.biz")
+     (send-mail-function 'sendmail-send-it)
+     ;; (smtpmail-smtp-user "jeremiah@legit.biz")
+     ;; (smtpmail-local-domain "legit.biz")
+     ;; (smtpmail-default-smtp-server "smtp.fastmail.com")
+     ;; (smtpmail-smtp-server "smtp.fastmail.com")
+     ;; (smtpmail-smtp-service 465)
+     (sendmail-program "/usr/bin/msmtp")
+     (user-full-name "Jeremiah Peschka")
+     )))
+
+
+(defun my-mu4e-set-account ()
+"Set the account for composing a message.
+This function is taken from:
+https://www.djcbsoftware.nl/code/mu/mu4e/Multiple-accounts.html"
+(let* ((account
+        (if mu4e-compose-parent-message
+            (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+              (string-match "/\\(.*?\\)/" maildir)
+              (match-string 1 maildir))
+          (completing-read (format "Compose with account: (%s) "
+                                   (mapconcat #'(lambda (var) (car var))
+                                              my-mu4e-account-alist "/"))
+                           (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                           nil t nil nil (caar my-mu4e-account-alist))))
+       (account-vars (cdr (assoc account my-mu4e-account-alist))))
+  (if account-vars
+      (mapc #'(lambda (var)
+                (set (car var) (cadr var)))
+            account-vars)
+    (error "No email account found"))))
+
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+
+;; The following convinces offlineimap to really really delete things that we've
+;; already deleted inside of mu4e
+(defun remove-nth-element (nth list)
+"Remove the NTH element of a LIST."
+  (if (zerop nth) (cdr list)
+    (let ((last (nthcdr (1- nth) list)))
+      (setcdr last (cddr last))
+      list)))
+(setq mu4e-marks (remove-nth-element 5 mu4e-marks))
+(add-to-list 'mu4e-marks
+             '(trash
+               :char ("d" . "▼")
+               :prompt "dtrash"
+               :dyn-target (lambda (target msg) (mu4e-get-trash-folder msg))
+               :action (lambda (docid msg target)
+                         (mu4e~proc-move docid
+                                         (mu4e~mark-check-target target) "-N"))))
+
+;; Include a bookmark to open all of my inboxes
+(add-to-list 'mu4e-bookmarks
+       (make-mu4e-bookmark
+        :name "All Inboxes"
+        :query "maildir:/legit.biz/INBOX OR maildir:/pdx.edu/INBOX"
+        :key ?i))
+
+
+
 
 ;;; corgmacs-custom.el ends here
