22fc6b2c3a989ee668eb30ae4afb475513dcc747
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2020-02-17 17:43:15 -0800

    Removed ZSH plugins

--- zsh/plugins/jp-aliases/jp-aliases.plugin.zsh
+++
@@ -1,2 +0,0 @@
-alias emct='emacsclient -a "" -t "$@"'
-alias findgrep='find . -type f -print0 | xargs -0 grep -I -H -i -n --color=always'
--- zsh/plugins/jp-oracle/jp-oracle.plugin.zsh
+++
@@ -1,8 +0,0 @@
-## Oracle settings
-export ORACLE_HOME=/usr/lib/oracle/12.1/client64
-export ORACLE_HOSTNAME=oracle12c01.localdomain
-export ORACLE_UNQNAME=cdb1
-export ORACLE_SID=cdb1
-# necessary for sqlplus to start
-export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib/:$LD_LIBRARY_PATH
-export PATH=$PATH:$ORACLE_HOME/bin
